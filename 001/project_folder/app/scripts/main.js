a1b68493b7e392342647d021c6b5759cac4233a5
rmisio <rmisio@users.noreply.github.com>
2015-03-11 14:06:25 -0700

    JS for page slide navigation

--- 001/project_folder/app/scripts/main.js
+++ 001/project_folder/app/scripts/main.js
@@ -1,21 +1,54 @@
 (function() {
-    var $slideContainer = $('.device-explorer .page-section-inner');
+    var $deviceExplorer = $('.device-explorer'),
+        $slideContainer = $('.page-section-inner', $deviceExplorer),
+        $slides = $('.slide', $slideContainer),
+        $slideNavButtons = $('.slide-nav span', $deviceExplorer);
+
+    $('a[href=#]').click(function() {
+        return false;
+    });
 
     // If below the largest break point, adjust the height of the
-    // Slide Container to be a percentage of it's width
+    // slides and their container to be a percentage of their width
     // (i.e. maintain the ratio of 1216 X 789)
-    function setSlideContainerHeight() {
+    function setSlideHeights() {
         if (Modernizr.mq('(max-width: 1200px)')) {
-            $slideContainer.height($slideContainer.width() * (789 / 1216));
+            $slideContainer.add($slides)
+                .height($slideContainer.width() * (789 / 1216));
         } else {
-            $slideContainer.css('height', '');
+            $slideContainer.add($slides)
+                .css('height', '');
         }
     }
-    setSlideContainerHeight();
+    setSlideHeights();
 
     $(window).resize(
         _.throttle(function() {
-            setSlideContainerHeight();
+            setSlideHeights();
         }, 100)
     );
+
+    function pageSlideNavigate(index) {
+        if (typeof index === 'number') {
+            $slideNavButtons.filter('.active')
+                .removeClass('active');
+
+            $slideContainer.animate({
+                scrollTop: $slideContainer.height() * index
+            }, 400, function() {
+                $slideNavButtons.eq(index)
+                    .addClass('active');
+            });
+        }
+    }
+
+    $('.device-note-4 a', $deviceExplorer).click(function() {
+        pageSlideNavigate(1);
+    });
+
+    $slideNavButtons.click(function(e) {
+        pageSlideNavigate(
+            $(this).parents('li').index()
+        );
+    });
 })();
