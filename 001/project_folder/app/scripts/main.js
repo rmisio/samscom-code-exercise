14c228bc9907a50643f59efa8323ab19bf417f21
rmisio <rmisio@users.noreply.github.com>
2015-03-10 18:35:18 -0700

    continuing to build out intro slide + js to set height of slide container

--- 001/project_folder/app/scripts/main.js
+++ 001/project_folder/app/scripts/main.js
@@ -1,1 +1,21 @@
-console.log('\'Allo \'Allo!');
+(function() {
+    var $slideContainer = $('.slide-container');
+
+    // If below the largest break point, adjust the height of the
+    // Slide Container to be a percentage of it's width
+    // (i.e. maintain the ratio of 1216 X 789)
+    function setSlideContainerHeight() {
+        if (Modernizr.mq('(max-width: 1200px)')) {
+            $slideContainer.height($slideContainer.width() * (789 / 1216));
+        } else {
+            $slideContainer.css('height', '');
+        }
+    }
+    setSlideContainerHeight();
+
+    $(window).resize(
+        _.throttle(function() {
+            setSlideContainerHeight();
+        }, 100)
+    );
+})();
